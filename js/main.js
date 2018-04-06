22e49d7ba90a6a0dea1d1f6ee050243dceacbc9e
ValentinEcoleSRP <valentinleroy.ecolesrp@gmail.com>
2018-04-06 22:28:47 +0200

    TestAxeCibles

---
+++ js/main.js
@@ -0,0 +1,27 @@
+/*var i = 0;
+var color = [];
+var time = 3000;
+
+
+// Color List 
+
+color[0] = '../img/green.jpg';
+color[1] = '../img/red.jpg';
+color[2] = '../img/blue.jpg';
+color[3] = '../img/black.jpg';
+
+// Change color
+
+function changeColor(){
+	document.slide.src = color[i];
+
+	if(i < color.length - 1){
+		i++;
+	}else{
+		i = 0;
+	}
+
+	setTimeout("changeColor()", time);
+}
+
+window.onload = changeColor;*/
